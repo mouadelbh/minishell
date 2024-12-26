d17dd473b63bf5b0048c38809f4e98fc4e26cada
mel-bouh <mel-bouh@c4r7p1.1337.ma>
2024-12-26 14:04:47 +0100

    fixed norminette

--- parsing/expand_utils.c
+++ parsing/expand_utils.c
@@ -6,7 +6,7 @@
 /*   By: mel-bouh <mel-bouh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 14:35:27 by mel-bouh          #+#    #+#             */
-/*   Updated: 2024/12/25 16:26:28 by mel-bouh         ###   ########.fr       */
+/*   Updated: 2024/12/26 14:00:56 by mel-bouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,84 +28,47 @@ void	reset_expand(char *str)
 	}
 }
 
-// int	get_env_length(t_list *env, int ca)
-// {
-// 	int	env_len;
+int	find(char *tmp, int i, t_list *env, int *size)
+{
+	int	j;
 
-// 	env_len = 0;
-// 	while (ca--)
-// 		env = env->next;
-// 	while (env->content[env_len] && env->content[env_len] != '=')
-// 		env_len++;
-// 	return (env_len + 1);
-// }
+	j = 0;
+	*size = 0;
+	i++;
+	while (!check_case(tmp, i + *size))
+		*size += 1;
+	tmp += i;
+	while (env)
+	{
+		if (!ft_strncmp(tmp, env->content, *size) && env->content[*size] == '=')
+			return (j);
+		env = env->next;
+		j++;
+	}
+	return (-1);
+}
 
-// char	*delete(char *tmp, int size)
-// {
-// 	char	*new;
-// 	int		j;
-// 	int		i;
+char	*fill_string(char *tmp, t_list *env, int env_len, int size)
+{
+	int		i;
+	int		j;
+	char	*new;
 
-// 	new = malloc(ft_strlen(tmp) + 1);
-// 	if (!new)
-// 		return (NULL);
-// 	i = 0;
-// 	while (tmp[i] && tmp[i] != -1)
-// 	{
-// 		new[i] = tmp[i];
-// 		i++;
-// 	}
-// 	j = i;
-// 	i = i + size + 1;
-// 	while (tmp[i])
-// 		new[j++] = tmp[i++];
-// 	new[j] = '\0';
-// 	free(tmp);
-// 	return (new);
-// }
-
-// int	alloc_exit(char *str, int exit)
-// {
-// 	int	size;
-// 	int	len;
-
-// 	len = 0;
-// 	if (exit <= 0)
-// 		len = 1;
-// 	while (exit)
-// 	{
-// 		len++;
-// 		exit /= 10;
-// 	}
-// 	size = ft_strlen(str) + len;
-// 	return (size);
-// }
-
-// char	*expand_exit(char *str, int i, int exit)
-// {
-// 	int		size;
-// 	int		j;
-// 	int		k;
-// 	char	*tmp;
-// 	char	*new;
-
-// 	size = alloc_exit(str, exit);
-// 	new = malloc(size + 1);
-// 	if (!new)
-// 		return (NULL);
-// 	j = 0;
-// 	k = 0;
-// 	while (j < i)
-// 	{
-// 		new[j] = str[j];
-// 		j++;
-// 	}
-// 	tmp = ft_itoa(exit);
-// 	while (tmp[k])
-// 		new[j++] = tmp[k++];
-// 	i += 2;
-// 	while (str[i])
-// 		new[j++] = str[i++];
-// 	new[j] = '\0';
-// 	return (free(str), free(tmp), new);
-// }
+	i = 0;
+	j = 0;
+	new = malloc(ft_strlen(tmp) + ft_strlen(env->content + env_len) - size + 1);
+	if (!new)
+		return (NULL);
+	while (tmp[i] && tmp[i] != -1)
+	{
+		new[i] = tmp[i];
+		i++;
+	}
+	j = i + size + 1;
+	while (env->content[env_len])
+		new[i++] = env->content[env_len++];
+	while (tmp[j])
+		new[i++] = tmp[j++];
+	new[i] = '\0';
+	return (new);
+}

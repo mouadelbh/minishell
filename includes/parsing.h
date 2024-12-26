01e14a564b285e88d48e03621809602d0b29a5ca
mel-bouh <mel-bouh@c4r7p1.1337.ma>
2024-12-26 18:49:58 +0100

    fixed header norminette

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: mel-bouh <mel-bouh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:56:42 by mel-bouh          #+#    #+#             */
-/*   Updated: 2024/12/26 16:15:34 by mel-bouh         ###   ########.fr       */
+/*   Updated: 2024/12/26 18:49:11 by mel-bouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,43 +22,44 @@
 # include <readline/history.h>
 # include "minishell.h"
 
-#define SUCCESS 0
-#define GENERAL_ERROR 1
-#define PARSE_ERROR 2
-#define COMMAND_INVOKED_CANNOT_EXECUTE 126
-#define COMMAND_NOT_FOUND 127
-#define INVALID_EXIT_ARGUMENT 128
-#define EXIT_OUT_OF_RANGE 255
-#define CTRL_C 130
+# define SUCCESS 0
+# define GENERAL_ERROR 1
+# define PARSE_ERROR 2
+# define COMMAND_INVOKED_CANNOT_EXECUTE 126
+# define COMMAND_NOT_FOUND 127
+# define INVALID_EXIT_ARGUMENT 128
+# define EXIT_OUT_OF_RANGE 255
+# define CTRL_C 130
 
 # define PATH 4096
-typedef enum
+
+typedef enum e_token
 {
-	NONE,		// 0
-	PIPE,		// 1
-	REDIR_IN,	// 2
-	REDIR_OUT,	// 3
-	APPEND,		// 4
-	HEREDOC,	// 5
-	EXPORT,		// 6
-	ARG,		// 7
-	CMD			// 8
+	NONE,
+	PIPE,
+	REDIR_IN,
+	REDIR_OUT,
+	APPEND,
+	HEREDOC,
+	EXPORT,
+	ARG,
+	CMD
 }	t_token;
 
-typedef struct s_cmd t_cmd;
-typedef struct s_data t_data;
+typedef struct s_cmd	t_cmd;
+typedef struct s_data	t_data;
 
-#define BUFFER_SIZE	4098
+# define BUFFER_SIZE	4098
 
-typedef struct	s_expand
+typedef struct s_expand
 {
 	int		size;
 	int		ca;
 }	t_expand;
 
-typedef struct	s_token
+typedef struct s_token
 {
-	char 			**str;
+	char			**str;
 	t_token			type;
 	struct s_token	*prev;
 	struct s_token	*next;
@@ -91,7 +92,7 @@ void	execute_signal(t_data *data, int temp_fd, char *temp_file);
 int		set_exit_status(int status);
 int		count_arguments(char **arg, int *i, t_line *tmp);
 int		print_error(char *str);
-int		parse(char *line, t_line **head, t_list *env,t_data *ex_data);
+int		parse(char *line, t_line **head, t_list *env, t_data *ex_data);
 int		check_token(int c);
 int		checkquotes(char *line, t_data *data);
 int		checkspaces(char *line);
@@ -99,7 +100,7 @@ int		quotes_open(char *s, int i);
 int		is_space(int c);
 int		isredir(int i);
 int		checkspaces(char *line);
-int 	is_empty(char *str);
+int		is_empty(char *str);
 int		count_words(const char *s, char c);
 int		alloc_exit(char *str, int exit);
 int		empty_node(t_line *node);
